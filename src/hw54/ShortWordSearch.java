8ed2606dffd4575993e674ddb405c4a8ad1f23be
Leon Zuev <loonatune@gmail.com>
2023-04-05 05:42:06 +0300

    Add input task2, ShortWordSearch

--- src/hw54/ShortWordSearch.java
+++ src/hw54/ShortWordSearch.java
@@ -7,5 +7,25 @@ package hw54;
 Оценить временную и пространственную сложность алгоритма.
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ShortWordSearch {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите количество слов: ");
+    int num = Integer.parseInt(br.readLine());
+    String[] wordList = new String[num];
+
+    System.out.println("Введите слова, каждое в новой строке: ");
+    int i;
+    for (i = 0; i < num; ++i) {
+      wordList[i] = br.readLine();
+    }
+    for (String word : wordList) {
+      System.out.println(word);
+    }
+  }
 }
