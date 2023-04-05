613e65bdc987a6011614e0dbcc750fc8909584a2
Leon Zuev <loonatune@gmail.com>
2023-04-05 03:19:10 +0300

    Finish task1, EvenIndexMin with wrong print

--- src/hw54/EvenIndexMin.java
+++ src/hw54/EvenIndexMin.java
@@ -1,38 +1,40 @@
 package hw54;
 
-import cw54.Utils;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
   /*
   Найти минимальное чётное число в последовательности положительных чисел
   или вывести -1, если такого числа не существует.
   Оценить временную и пространственную сложность алгоритма.
+
+  не понял, откуда брать саму последовательность
    */
 
 public class EvenIndexMin {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    List<Integer> numbers = Utils.readList();
 
     System.out.println("Ведите последовательность чисел:");
-    numbers = Collections.singletonList(Integer.parseInt(br.readLine()));
+    String[] input = br.readLine().split(" ");
+    List<Integer> numbers = new ArrayList<>();
 
-    if (i != -1) {
-      System.out.println("Минимальное чётное число: ");
+    for (String number : input) {
+      numbers.add(Integer.parseInt(number));
     }
+
+    System.out.println(search(numbers));
   }
 
   public static int search(List<Integer> numbers) {
-    for (int i = 0; i < numbers.size(); ++i) {
-      if (numbers.get(i)/2 == 0) {
-        return i;
+    for (int number : numbers) {
+      if (number % 2 == 0) {
+        System.out.println(number);
       }
     }
-
+    return -1;
   }
 }
 
