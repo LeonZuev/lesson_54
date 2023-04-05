86c3fb134dd49b446a5f980886046a4efc782853
Leon Zuev <loonatune@gmail.com>
2023-04-05 14:39:19 +0300

    Add BooleanToString

---
+++ src/hw54/BooleanToString.java
@@ -0,0 +1,19 @@
+package hw54;
+
+public class BooleanToString {
+  public static void main(String[] args) {
+    boolean booleanValue = true;
+    System.out.println(convert(booleanValue));
+  }
+
+  public static String convert(boolean b) {
+    return Boolean.toString(b);
+    /*return b ? "true" : "false";*/
+    /*return String.valueOf(b);*/
+    /*return b+"";*/
+  }
+
+ /* public static String convert(boolean value){
+    return String.valueOf(value);
+  }*/
+}
